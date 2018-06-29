0b38fa13dda0b2d9f282532d8c7198232c02a6eb
Joe Ray <jray@etsy.com>
2018-06-29 20:49:45 +0000

    Support fzf installs in home directory

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,19 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* && -d /usr/local/opt/fzf/bin ]]; then
   export PATH="$PATH:/usr/local/opt/fzf/bin"
+fi
+if [[ ! "$PATH" == *~/.fzf/bin* && -d ~/.fzf/bin ]]; then
+  export PATH="$PATH:~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* && -f /usr/local/opt/fzf/shell/completion.zsh ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* && -f $HOME/.fzf/shell/completion.zsh ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+[[ -f /usr/local/opt/fzf/shell/completion.zsh ]] && source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+[[ -f $HOME/.fzf/shell/completion.zsh ]] && source "$HOME/.fzf/shell/key-bindings.zsh"
 
